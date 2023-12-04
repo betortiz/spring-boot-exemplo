9e2f5cff31ed7c432770a6af7436fba6762be034
betortiz <alberto.ortiz.medeiros@gmail.com>
2023-12-04 17:19:21 -0400

    spring-project

---
+++ src/main/java/br/com/treinaweb/springbootexemplo/SpringbootExemploApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.springbootexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/springbootexemplo/SpringbootExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.springbootexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
